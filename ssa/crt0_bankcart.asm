7599ab751dc69c21f9657a17faf628fd454ae3a4
Tursi <tacobell@harmlesslion.com>
2023-11-01 04:09:22 -0600

    - update for new banked processor - all banks (except 1) now get a header - move rest of boss palettes to fixed bank temporarily - need to deal with fixed being too large

---
+++ ssa/crt0_bankcart.asm
@@ -0,0 +1,132 @@
+* Bank cart header by Tursi
+* based on original CRT0 by Insomniac for GCC ELF
+
+ def _start
+ def _init_data
+ 
+* cartridge header here - we dont expect a normal startup entry
+* We can reserve 44 bytes for the header on every page - that must take us to the bank switch!
+    aorg >6000
+    
+* because many bytes in the standard header are unused, and location is well known,
+* we store our init data there. That way the CRT length can change without needing
+* to change the elf2cart tool
+    data 0xaa01,0x0100      * header, version, [program count, unused] - [] used as data start in RAM
+    data 0x0000,_prog       * [power up list], program list - [] used as data address in ROM
+    data 0x0000,0x0000      * [DSR list], [subprogram list] - [] used as data size, [] then is bss start
+    data 0x0000,0x0000      * not standard header - bss length, fixed program start
+
+* program list
+_prog:
+    data 0x0000             * next program
+    data _start             * entry point
+    byte 21                 * length of following name
+    text 'SUPER SPACE ACER 2023'
+    even
+
+* 40 bytes used till this point (note title length!)
+* Pad the above if necessary to get to 42 bytes so the cartridge headers work.
+
+* Entry point for C runtime initilization code
+*
+* This will set initial values for data memory and otherwise create
+* the conditions needed for the C environment
+_start:
+  clr @>6000          * force bank switch to the first page - must end at exactly 46 bytes!
+
+* Disable interrupts
+*
+* This will disable console reset via keyboard, music playback,
+* sprite automotion, etc. But will make the environment much simpler
+* to work in. Users can re-enable interrupts later if they wish.
+  limi 0
+
+* Set initial workspace
+*
+* The registers will be located at the start of scratchpad memory 
+  lwpi 0x8300
+
+* Initialize data segment to known values
+  li   r0,0x6002       * Point into the standard header as above
+  mov  *r0+, r1        * R1 = Start of data section
+  mov  *r0+, r2        * R2 = Location of initial data
+  inct r0              * skip program list
+  mov  *r0+, r3        * R3 = Size of data section
+  jeq  data_copy_end   * If size is 0, skip the loop
+  
+  * Copy loop for data initialization
+data_copy_top:
+  mov  *r2+, *r1+      
+  dect r3
+  jgt  data_copy_top
+data_copy_end:
+
+* Initialize BSS segment to zeroes
+  mov  *r0+, r1        * R1 = Start of BSS section
+  mov  *r0, r2         * R2 = Size of BSS section
+  jeq  bss_clear_end   * If size is 0, skip the loop
+
+  * Clearing loop for BSS initialization
+bss_clear_top:
+  clr  *r1+
+  dect r2
+  jgt  bss_clear_top
+bss_clear_end:
+
+* now copy the rest of the fixed data into RAM
+* we are lazy here and assume a load address of 0xA000
+* we also assume the rest of this page and all of page 1
+* for (almost) 16k, though right now itd be nice to have
+* all 24k somehow... if we can free up one more page ;)
+* there's enough room, if it's possible to move things
+* around. Anyway... to do this right we are going to
+* need to work from scratchpad - copy into >8320
+
+  li r0,>8320
+  li r1,cpcode
+cpsclp:
+  mov *r1+,*r0+
+  ci r1,cpend
+  jne cpsclp
+
+* destination in RAM
+  li r0,>a000  
+
+* start address in header block
+  mov @>600E,R1
+
+* do the copy - we will return
+  bl @>8320
+
+* Create the stack
+  li sp, 0x4000
+
+* Start running C code
+  bl @main
+
+* reset the console on return
+  blwp @0x0000
+
+cpcode:
+* start from R1, copy the rest of the page,
+* then switch the page and copy the whole thing,
+* then reset bank and return. Only copies first 16k
+* (minus the header bits)
+cplp:
+  mov *r1+,*r0+     * first copy loop
+  ci r1,>8000
+  jne cplp
+
+  li r0,>6000       * restart copy
+  clr @>6002        * but on page 1
+
+cplp2:
+  mov *r1+,*r0+     * second copy loop
+  ci r1,>8000
+  jne cplp2
+
+  clr @>6000        * restore page 0
+  b *r11            * back to caller
+cpend:
+
+
